fe70c1e9773cddb067a8bbf4c7f04c338a6ab9ec
Lam Nhan <nhan.lh@raksul.com>
2022-12-03 18:24:14 +0700

    init

---
+++ demo-2/pages/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="page">
+
+    <div id="container">
+
+      <section id="emojis">
+        <h3>Example 1: Emojis</h3>
+        <p>Transform oriented (position, size, rotation, ...).</p>
+        <Emojis></Emojis>
+      </section>
+  
+      <section id="neon-light">
+        <h3>Example 2: Neon Light</h3>
+        <p>Presentation oriented (color, shadow, opacity, ...).</p>
+        <NeonLight></NeonLight>
+      </section>
+  
+      <section id="logos">
+        <h3>Example 3: Logos</h3>
+        <p>More advance and interactive.</p>
+        <Logos></Logos>
+      </section>
+
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+declare const gsap: any
+gsap.config({ trialWarn: false })
+
+export default Vue.extend()
+</script>
+
+<style lang="scss" scoped>
+
+  .page {
+    width: 100%;
+    min-width: 100vw;
+    height: 100%;
+    min-height: 100vh;
+    background: #f1f1f1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 2rem;
+  }
+
+  #container {
+    width: 100%;
+    max-width: 500px;
+    background: #ffffff;
+    padding: 2rem;
+    border-radius: 1rem;
+  }
+
+  section {
+    margin-bottom: 4rem;
+
+    &:last-child {
+      margin-bottom: 2rem;
+    }
+
+    h3 {
+      margin: 0;
+    }
+
+    p {
+      font-size: .9rem;
+      margin-top: .25rem;
+      margin-bottom: 2rem;
+      color: #777777;
+      font-style: italic;
+    }
+  }
+
+</style>
